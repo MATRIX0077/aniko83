8ced2ec5c0b0455f4d8ff259b253d7a4198fea38
MATRIX0077 <70593541+MATRIX0077@users.noreply.github.com>
2020-09-03 19:09:16 +0200

    init
    init

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('TREVOR is online!');
+});
+
+
+client.login('NzUwMDgxMDM0NTk0Mjg3NjI2.X01VZg.GrYZU4Vg3jDgrhURxngTLrtNVX8');
